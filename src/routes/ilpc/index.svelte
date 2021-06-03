8b7db3db82f9f6295ef3410221e633dc9d986654
InCogNiTo124 <msmetko@msmetko.xyz>
2021-06-03 22:07:35 +0200

    Separate sections in ILPC

--- src/routes/ilpc/index.svelte
+++ src/routes/ilpc/index.svelte
@@ -8,10 +8,22 @@
       url: "",
       data: [
         "In 2021 I held a Python programming language course <i>pro bono</i>. I named it \"InCogNiTo124's Little Python Course\", for my online username at a number of websites. Although intended primarily for the students of my faculty, it was watched by numerous other people from all around the region, from Vienna, Austria to Belgrade, Serbia. It was very well recieved, with numerous good reviews and thanks for my teaching style, interesting examples and homework assignments",
-        "I did it primarily because there aren't enough materials for Python at my faculty, and especially in my native language, and secondarily, because I wanted to try myself at teaching, to push myself out of the comfort zone. It was an excellent creative experience, as I had to deal with a lot of problems, such as teaching infrastructure, interesting but useful-in-real-world examples, and making and organizing curriculum and lectures themselves.",
-        `The course's official repository, including the lecture recordings and homeworks (both in Croatian) can be found <a class=${INLINE_CLASS} ${TARGET_BLANK} href="https://github.com/studosi-fer/ILPC">here</a>.`
       ],
     },
+    {
+      title: "Why I did it",
+      url: "",
+      data: [
+        "I did it primarily because there aren't enough materials for Python at my faculty, and especially in my native language, and secondarily, because I wanted to try myself at teaching, to push myself out of the comfort zone. It was an excellent creative experience, as I had to deal with a lot of problems, such as teaching infrastructure, interesting but useful-in-real-world examples, and making and organizing curriculum and lectures themselves.",
+    ]
+  },
+  {
+    title: "Materials",
+    url: "",
+    data: [
+      `The course's official repository, including the lecture recordings and homeworks (both in Croatian) can be found <a class=${INLINE_CLASS} ${TARGET_BLANK} href="https://github.com/studosi-fer/ILPC">here</a>.`
+    ]
+  }
 ];
 </script>
 
