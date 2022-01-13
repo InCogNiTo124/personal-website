597e4d316a85e94c63c8510dc2ebdc4d3fdb72af
Amalija Ramljak <amalija@netgen.io>
2022-01-13 23:17:54 +0100

    issue-6 update loader

--- src/components/Loader.svelte
+++ src/components/Loader.svelte
@@ -1,104 +1,117 @@
-<div class="lds-default">
-  <div />
-  <div />
-  <div />
-  <div />
-  <div />
-  <div />
-  <div />
-  <div />
-  <div />
-  <div />
-  <div />
-  <div />
+<div class="loader-parent">
+  <div class="loader-content">
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
 </div>
 
-<style>
-  .lds-default {
-    display: inline-block;
+<style type="text/css">
+  .loader-parent {
+    width: 100%;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    overflow: hidden;
+    background: none;
+  }
+  .loader-content {
+    width: 200px;
+    height: 200px;
     position: relative;
-    left: 50%;
-    transform: translate(-50%, 0);
-    width: 80px;
-    height: 80px;
+    transform: translateZ(0) scale(1);
+    backface-visibility: hidden;
+    transform-origin: 0 0;
   }
-  .lds-default div {
-    position: absolute;
-    width: 6px;
-    height: 6px;
-    border-radius: 50%;
-    background-image: radial-gradient(red, white 80%);
-    animation: lds-default 1.2s linear infinite;
+  .loader-content div {
+    box-sizing: content-box;
   }
-  .lds-default div:nth-child(1) {
-    animation-delay: 0s;
-    top: 37px;
-    left: 66px;
-  }
-  .lds-default div:nth-child(2) {
-    animation-delay: -0.1s;
-    top: 22px;
-    left: 62px;
-  }
-  .lds-default div:nth-child(3) {
-    animation-delay: -0.2s;
-    top: 11px;
-    left: 52px;
-  }
-  .lds-default div:nth-child(4) {
-    animation-delay: -0.3s;
-    top: 7px;
-    left: 37px;
-  }
-  .lds-default div:nth-child(5) {
-    animation-delay: -0.4s;
-    top: 11px;
-    left: 22px;
-  }
-  .lds-default div:nth-child(6) {
-    animation-delay: -0.5s;
-    top: 22px;
-    left: 11px;
-  }
-  .lds-default div:nth-child(7) {
-    animation-delay: -0.6s;
-    top: 37px;
-    left: 7px;
-  }
-  .lds-default div:nth-child(8) {
-    animation-delay: -0.7s;
-    top: 52px;
-    left: 11px;
-  }
-  .lds-default div:nth-child(9) {
-    animation-delay: -0.8s;
-    top: 62px;
-    left: 22px;
-  }
-  .lds-default div:nth-child(10) {
-    animation-delay: -0.9s;
-    top: 66px;
-    left: 37px;
-  }
-  .lds-default div:nth-child(11) {
-    animation-delay: -1s;
-    top: 62px;
-    left: 52px;
-  }
-  .lds-default div:nth-child(12) {
-    animation-delay: -1.1s;
-    top: 52px;
-    left: 62px;
-  }
-  @keyframes lds-default {
-    0%,
-    20%,
-    80%,
+  @keyframes loader-content {
+    0% {
+      opacity: 1;
+    }
     100% {
-      transform: scale(1);
+      opacity: 0;
     }
-    50% {
-      transform: scale(1.5);
-    }
+  }
+  .loader-content div {
+    left: 94px;
+    top: 46px;
+    position: absolute;
+    animation: loader-content linear 1s infinite;
+    background: #da0000;
+    width: 12px;
+    height: 12px;
+    border-radius: 6px / 6px;
+    transform-origin: 6px 54px;
+  }
+  .loader-content div:nth-child(1) {
+    transform: rotate(0deg);
+    animation-delay: -0.9166666666666666s;
+    background: #da0000;
+  }
+  .loader-content div:nth-child(2) {
+    transform: rotate(30deg);
+    animation-delay: -0.8333333333333334s;
+    background: #da0000;
+  }
+  .loader-content div:nth-child(3) {
+    transform: rotate(60deg);
+    animation-delay: -0.75s;
+    background: #da0000;
+  }
+  .loader-content div:nth-child(4) {
+    transform: rotate(90deg);
+    animation-delay: -0.6666666666666666s;
+    background: #da0000;
+  }
+  .loader-content div:nth-child(5) {
+    transform: rotate(120deg);
+    animation-delay: -0.5833333333333334s;
+    background: #da0000;
+  }
+  .loader-content div:nth-child(6) {
+    transform: rotate(150deg);
+    animation-delay: -0.5s;
+    background: #da0000;
+  }
+  .loader-content div:nth-child(7) {
+    transform: rotate(180deg);
+    animation-delay: -0.4166666666666667s;
+    background: #da0000;
+  }
+  .loader-content div:nth-child(8) {
+    transform: rotate(210deg);
+    animation-delay: -0.3333333333333333s;
+    background: #da0000;
+  }
+  .loader-content div:nth-child(9) {
+    transform: rotate(240deg);
+    animation-delay: -0.25s;
+    background: #da0000;
+  }
+  .loader-content div:nth-child(10) {
+    transform: rotate(270deg);
+    animation-delay: -0.16666666666666666s;
+    background: #da0000;
+  }
+  .loader-content div:nth-child(11) {
+    transform: rotate(300deg);
+    animation-delay: -0.08333333333333333s;
+    background: #da0000;
+  }
+  .loader-content div:nth-child(12) {
+    transform: rotate(330deg);
+    animation-delay: 0s;
+    background: #da0000;
   }
 </style>
