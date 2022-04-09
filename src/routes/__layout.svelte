c530ee42599987a13f3bfe704d44b88c504e8157
amalija-ramljak <amalija@netgen.io>
2022-04-09 22:06:46 +0200

    rename files

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { onMount } from 'svelte';
+  import { browser } from '$app/env';
+  import { LIGHT, storageTheme } from '$slib/utils';
+  import { theme } from '$slib/stores/theme_store';
+  import Nav from '$lib/components/Nav.svelte';
+  import Header from '$lib/components/Header.svelte';
+
+  let segment = '';
+  onMount(() => {
+    if (browser) {
+      theme.useLocalStorage();
+      theme.subscribe((newval) => {
+        document.getElementById('body').setAttribute('class', newval);
+      });
+      document
+        .getElementById('body')
+        .setAttribute('class', localStorage.getItem(storageTheme) || LIGHT);
+    }
+  });
+</script>
+
+<Header />
+<div id="app">
+  <Nav {segment} />
+  <div id="content">
+    <div id="main">
+      <slot />
+    </div>
+  </div>
+</div>
