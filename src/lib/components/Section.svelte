a819949c38d13f20b3919a6e2f8bc242c718eda2
amalija-ramljak <amalija@netgen.io>
2022-04-10 12:43:55 +0200

    update to use submodule section

--- src/lib/components/Section.svelte
+++ src/lib/components/Section.svelte
@@ -1,70 +1,18 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { browser } from '$app/env';
-  import { BLANK, LIGHT, storageTheme } from '$slib/utils';
-  import { theme } from '$slib/stores/theme_store';
-  export let url, title, data;
+  import Section from '$slib/components/Sections/Section.svelte';
+  import { BLANK } from '$slib/utils';
 
-  let themeValue = LIGHT;
-
-  onMount(() => {
-    if (browser) {
-      theme.subscribe((newval) => {
-        themeValue = newval;
-      });
-
-      themeValue = localStorage.getItem(storageTheme) || LIGHT;
-    }
-  });
+  export let url: string;
+  export let title: string;
+  export let data: string[];
 </script>
 
-<div class="section {themeValue}">
-  <h3>
-    {#if url}
-      <a href={url} target={BLANK}>
-        <div>{title}</div>
-      </a>
-    {:else}
-      <div>{title}</div>
-    {/if}
-  </h3>
-  <div>
+<Section {url} {title} urlTarget={BLANK}>
+  <div slot="body">
     {#each data as paragraph}
       <p>
         {@html paragraph}
       </p>
     {/each}
   </div>
-</div>
-
-<style scoped lang="css">
-  .section {
-    padding: 20px 0;
-    margin: 10px 0;
-    padding: 10px 5px;
-    padding-left: 15px;
-    opacity: 1;
-  }
-
-  .section h3 {
-    width: max-content;
-  }
-  
-  .section h3 div {
-    padding: 5px;
-    margin-bottom: 10px;
-    border-bottom: 1px solid black;
-  }
-
-  .dark-theme h3 div {
-    border-bottom: 1px solid white;
-  }
-
-  .section h3 div:hover {
-    border-color: rgba(218, 0, 0, 1);
-  }
-
-  .section div {
-    padding-left: 5px;
-  }
-</style>
+</Section>
