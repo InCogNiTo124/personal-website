d8ec9bbc124516762c8b364768f259c75af503e3
InCogNiTo124 <msmetko@msmetko.xyz>
2024-01-01 21:00:49 +0100

    Add missing `@sveltejs/vite-plugin-svelte` dependency

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const config = {
     // "Readable" is not exported by "__vite-browser-external", imported by "node_modules/axios/lib/helpers/formDataToStream.js".
     // "TextEncoder" is not exported by "__vite-browser-external", imported by "node_modules/axios/lib/helpers/formDataToStream.js".
     // I suppose this can be rid of once in future
-    include: ['path', 'http', 'https', 'zlib', 'stream', 'path', 'fs', 'os', 'tty'],
+    include: ['path', 'http', 'https', 'zlib', 'stream', 'path', 'fs', 'os', 'tty', 'util'],
   })],
   resolve: {
     alias: {
